d8bfba5b90a08e96f913c15c62c6954727301fad
Your Name <you@example.com>
2021-04-11 15:48:35 +0300

    commiting

---
+++ week2/day1/DailyChallange/script.js
@@ -0,0 +1,12 @@
+/* exercise 1
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(fruits.shift());
+console.log(fruits.sort());
+console.log(fruits.push("Kiwi"));
+fruits = fruits.slice(1);
+console.log(fruits);
+console.log(fruits.reverse()); */
+
+//exercise 2
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1][0]);
---
+++ week2/day1/xp/script.js
@@ -0,0 +1,7 @@
+//exercise 1
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(fruits.shift());
+console.log(fruits.sort());
+console.log(fruits.push("Kiwi"));
+fruits = (fruits.slice());
+console.log(fruits.slice(1));
