6e380a2888a34473a75e371837f02928d59e3167
Your Name <you@example.com>
2021-04-11 20:16:52 +0300

    commiting

---
+++ week2/day1/xpGold/script.js
@@ -0,0 +1,19 @@
+/*exercise1
+let me = ["my", "favorite", "color", "is", "blue"]
+console.log(me.join(" "));
+*/
+
+/*exercise2
+
+let str1 = "mix";
+let str2 = "pod";
+let str3 = (str2.slice(0, 2)) + (str1.slice(2)) + (' ') + (str1.slice(0, 2)) + (str2.slice(2));
+*/
+
+/*exercise */
+
+let num1 = (prompt(parseInt('num1')));
+let num2 = (prompt(parseInt('Num2')));
+/*var num1 = parseInt(x);
+var num2 = parseInt(y);*/
+alert(`your number is  ${num1} + ${num2} !`);
