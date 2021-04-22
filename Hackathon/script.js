5a6409657fc267a502dc49e7bd9f6108b74c8d19
Your Name <you@example.com>
2021-04-23 00:49:04 +0300

    commiting

--- Hackathon/script.js
+++ Hackathon/script.js
@@ -1,19 +1,7 @@
-// let score = document.getElementsByClassName("score");
-// let correct = document.getElementById('point');
-
-// score.onclick = function () {
-//     correct += 100;
-
-// }
-
-
-
 var myscore = document.getElementById('myscore');
 var zero = document.getElementById('counter');
 myscore.addEventListener('click', myfunction)
 
 function myfunction() {
     zero.innerHTML = +(zero.innerHTML) + 100;
-
-
 }
