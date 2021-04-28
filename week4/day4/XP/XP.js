e20d38effed7c903148c6ebe3e278f7d34c631ec
Your Name <you@example.com>
2021-04-28 19:42:39 +0300

    week 4 day 4

--- week4/day4/XP/XP.js
+++ week4/day4/XP/XP.js
@@ -42,27 +42,21 @@
 // exercise 3
 // Using this object const users = { user1: 18273, user2: 92833, user3: 90315 }
 
-// Using methods taught in class, turn the users object into an array:
-// Excepted output: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
-// FYI : The number is their ID number.
 
-// Modify the outcome of part 1, by multipling the user’s ID by 2.
-// Excepted output: [ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+const users = {
+    user1: 18273,
+    user2: 92833,
+    user3: 90315
+}
 
-// const users = {
-//     user1: 18273,
-//     user2: 92833,
-//     user3: 90315
-// }
+let user = Object.entries(users)
+console.log(user);
 
-// let user = Object.entries(users)
-// console.log(user);
+let outcome = user.map((i) => {
+    return (i[1]) * 2;
+})
 
-// let outcome = user.forEach((i) => {
-//     return (i[1]) * 2;
-// })
-
-// console.log(outcome);
+console.log(outcome);
 
 
 // exercise 4
@@ -80,45 +74,45 @@
 
 
 // exercise 5
-class Dog {
-    constructor(name) {
-        this.name = name;
-    }
-};
-//   Which constructor will successfully extend the Dog class?
-// 1
-class Labrador extends Dog {
-    constructor(name, size) {
-        this.size = size;
-    }
-};
+// class Dog {
+//     constructor(name) {
+//         this.name = name;
+//     }
+// };
+// //   Which constructor will successfully extend the Dog class?
+// // 1
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//         this.size = size;
+//     }
+// };
 
 
-// 2
-class Labrador extends Dog {
-    constructor(name, size) {
-        super(name);
-        this.size = size;
-    }
-};
+// // 2
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//         super(name);
+//         this.size = size;
+//     }
+// };
 
 
-// 3
-class Labrador extends Dog {
-    constructor(size) {
-        super(name);
-        this.size = size;
-    }
-};
+// // 3
+// class Labrador extends Dog {
+//     constructor(size) {
+//         super(name);
+//         this.size = size;
+//     }
+// };
 
 
-// 4
-class Labrador extends Dog {
-    constructor(name, size) {
-        this.name = name;
-        this.size = size;
-    }
-};
+// // 4
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//         this.name = name;
+//         this.size = size;
+//     }
+// };
 
 // the answer is: number two
 
